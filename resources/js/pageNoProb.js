1f997405cf6b40369375a3c954cb79cc2f56c18a
Laura Mandell <mandell@tamu.edu>
2019-10-23 09:32:55 -0500

    fixing page numbers

--- resources/js/pageNoProb.js
+++ resources/js/pageNoProb.js
@@ -1,7 +1,7 @@
 function pageNoFix() {
 	var pages = document.getElementsByClassName('pageNumber');
 	var main = document.querySelector('main');
-	var main_pos = main.getBoundingClientRect().left;
+	var main_pos = main.getBoundingClientRect().left + 30;
 	for (var x = 0; x < pages.length; x++) {
    	var rect = pages[x].getBoundingClientRect();
    	var left_margin = 0;
